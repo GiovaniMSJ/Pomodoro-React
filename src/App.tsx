28bd949c6f2ccf7a59f696ff7e68a96a26747e72
Giovani <giovanimoreirasantosjunior@gmail.com>
2025-05-08 19:35:04 -0300

    Criado o css

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Heading } from './components/Heading'
+import './styles/global.css'
+import './styles/theme.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+function App () {
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+    return (
+        <>
+            <Heading attr={123} attr2='String'>Olá mundo</Heading>
+            <Heading>Tudo certo</Heading>
+            <Heading>Com voçê</Heading>
+            <Heading>Em</Heading>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam pariatur voluptates sit, exercitationem eos, ex libero porro culpa facere, sint facilis ducimus quis fugit sequi reiciendis earum? Vitae, iure itaque?</p>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia, eos harum aut distinctio nesciunt quisquam facilis sint ex, esse, molestias quidem excepturi consequatur est nemo facere voluptatem necessitatibus velit a?</p>
+        </>
+    )
 }
 
-export default App
+export {App}
