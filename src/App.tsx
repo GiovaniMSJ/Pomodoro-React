cf9bb3570b7460c60f0764c8a933683d3d69a89d
Giovani <giovanimoreirasantosjunior@gmail.com>
2025-05-15 19:43:39 -0300

    Arquivo de container criados

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import { Heading } from './components/Heading'
+import { Container } from './components/Container'
 import './styles/global.css'
 import './styles/theme.css'
 
-function App () {
-
+export function App () {
 
     return (
-        <>
-            <Heading attr={123} attr2='String'>Olá mundo</Heading>
-            <Heading>Tudo certo</Heading>
-            <Heading>Com voçê</Heading>
-            <Heading>Em</Heading>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam pariatur voluptates sit, exercitationem eos, ex libero porro culpa facere, sint facilis ducimus quis fugit sequi reiciendis earum? Vitae, iure itaque?</p>
-            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia, eos harum aut distinctio nesciunt quisquam facilis sint ex, esse, molestias quidem excepturi consequatur est nemo facere voluptatem necessitatibus velit a?</p>
+        <>  
+        <Container>
+            Teste
+        </Container>
+
+        <Container>
+            MENU
+        </Container>
         </>
     )
 }
-
-export {App}
